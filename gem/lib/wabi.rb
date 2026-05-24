a9e710c05b43d847eec86b05a96b99fe3bc5d10c
OscarOrtega <ortegaoscar14@gmail.com>
2026-05-24 11:31:37 -0500

    feat(gem): auto-require Wabi::Base from wabi.rb

--- gem/lib/wabi.rb
+++ gem/lib/wabi.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "wabi/version"
+require_relative "wabi/base"
 
 module Wabi
   class Error < StandardError; end
--- gem/spec/wabi_spec.rb
+++ gem/spec/wabi_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe Wabi do
   it "defines a base Error class" do
     expect(Wabi::Error).to be < StandardError
   end
+
+  it "auto-loads Wabi::Base" do
+    expect(defined?(Wabi::Base)).to eq("constant")
+  end
 end
